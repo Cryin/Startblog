ea4d29c96e0a2c6f08c1a0a46c03091baa5eb695
lihu <lihu2@intra.nsfocus.com>
2016-08-18 16:47:15 +0800

    修改再linux下无法找到install.php问题，修复标签显示问题！

--- startblog/application/views/tag_index.php
+++ startblog/application/views/tag_index.php
@@ -1,7 +1,4 @@
-
-    <div class="col-sm-2" style="padding-top:20px">
-    </div>
-    <div class="col-sm-8" style="padding-top:20px">
+<div class="container col-sm-8 col-sm-offset-2" style="background-color: #FFF;margin-top:20px;padding-top:1px;border-radius: 8px;box-shadow:5px 5px 8px #DDDDDD,-5px -5px 8px #DDDDDD;">
       <center >
         <?php foreach ($data as $key => $value): ?>
             <?php echo anchor("/Tag/show/{$value['tag_id']}","<button type='button' class='btn btn-{$value['tag_button_type']} {$value['tag_size']}'>{$value['tag_name']}</button>","")?>
