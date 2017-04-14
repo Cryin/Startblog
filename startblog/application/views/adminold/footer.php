530ea8d6e5f1394bd90792274ea2b756b7d9c94f
lihu <lihu2@intra.nsfocus.com>
2017-04-14 11:35:23 +0800

    fix sql bug and search page bug

--- startblog/application/views/adminold/footer.php
+++
@@ -1,8 +0,0 @@
-    <script src="<?php echo base_url('/static/admin/js/amazeui.min.js')?>"></script>
-    <script src="<?php echo base_url('/static/admin/js/amazeui.datatables.min.js')?>"></script>
-    <script src="<?php echo base_url('/static/admin/js/dataTables.responsive.min.js')?>"></script>
-    <script src="<?php echo base_url('/static/admin/js/app.js')?>"></script>
-
-</body>
-
-</html>
